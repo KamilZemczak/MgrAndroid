c2787e8e9641dc0609cb404091445511d787098b
Kamil Zemczak <kamilzemb@gmail.com>
2019-05-05 20:25:40 +0200

    Inicjalizacja repozytorium.

---
+++ app/src/main/java/com/example/kamilzemczak/mgrandroid/activity/LoginActivity.java
@@ -0,0 +1,190 @@
+package com.example.kamilzemczak.mgrandroid.activity.activity_entry;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.kamilzemczak.mgrandroid.R;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+
+/**
+ * Klasa odpowiedzialna za logowanie użytkownika do portalu
+ */
+public class LoginActivity extends AppCompatActivity {
+
+    MySingleton singleton =  MySingleton.getInstance();
+
+    private EditText username, password;
+    private Button loginButton;
+
+    public static String userCurrentName, userCurrentSurname, userCurrentUsername, userCurrentEmail, userCurrentGender, userCurrentCity, userCurrentAbout;
+    public static Integer userCurrentId, userCurrentAge, userCurrentWeight, userCurrentHeight;
+
+    /**
+     * TODO
+     *
+     * @param savedInstanceState TODO
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_login);
+
+        username = (EditText) findViewById(R.id.loginActivity_etName);
+        password = (EditText) findViewById(R.id.loginActivity_etPassword);
+        loginButton = (Button) findViewById(R.id.loginActivity_bLogin);
+    }
+
+    /**
+     * Przesyła na serwer dane które użytkownik podał w formularzu logowanie
+     *
+     * @param view aktualny interfejs
+     */
+    public void login(View view) {
+        if (!validate()) {
+            loginFailed();
+            return;
+        }
+
+        String type = "login";
+        String username = this.username.getText().toString();
+        String password = this.password.getText().toString();
+        String result = null;
+        LoginBackgroundWorker loginBackgroundWorker = new LoginBackgroundWorker(this);
+        try {
+            result = loginBackgroundWorker.execute(type, username, password).get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        if (result.equals("Wrong.")) {
+            loginFailed();
+            this.username.setError("Niewłaściwy login lub hasło.");
+            this.password.setError("Niewłaściwy login lub hasło.");
+            return;
+        } else if (result.equals("Zalogowanie udane.")) {
+            getUserDetails();
+            loginSuccess();
+            startActivity(new Intent(this, WelcomeActivity.class));
+        }
+    }
+
+    /**
+     * TODO
+     */
+    public void getUserDetails() {
+        String username = this.username.getText().toString();
+        String type = "user_details";
+        UserBackgroundWorker userBackgroundWorker = new UserBackgroundWorker(this);
+        try {
+            User currentUser;
+            ObjectMapper mapper = new ObjectMapper();
+            String userJson = userBackgroundWorker.execute(type, username).get();
+            currentUser = mapper.readValue(userJson, User.class);
+            prepareUserData(currentUser);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (JsonParseException e) {
+            e.printStackTrace();
+        } catch (JsonMappingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * TODO
+     *
+     * @param currentUser
+     */
+    private void prepareUserData(User currentUser) {
+        userCurrentId = currentUser.getId();
+        userCurrentName = currentUser.getName();
+        singleton.setCurrentName(currentUser.getName());
+        singleton.setWeight(currentUser.getWeight());
+        singleton.setHeight(currentUser.getHeight());
+        singleton.setAbout(currentUser.getAbout());
+        singleton.setCity(currentUser.getCity());
+        userCurrentSurname = currentUser.getSurname();
+        userCurrentUsername = currentUser.getUsername();
+        userCurrentEmail = currentUser.getEmail();
+        userCurrentAge = currentUser.getAge();
+        userCurrentGender = currentUser.getGender();
+        if (currentUser.getWeight() != null) {
+            userCurrentWeight = currentUser.getWeight();
+        }
+        if (currentUser.getHeight() != null) {
+            userCurrentHeight = currentUser.getHeight();
+        }
+        if (currentUser.getCity() != null) {
+            userCurrentCity = currentUser.getCity();
+        }
+        if (currentUser.getAbout() != null) {
+            userCurrentAbout = currentUser.getAbout();
+        }
+    }
+
+    /**
+     * TODO
+     *
+     * @return
+     */
+    public boolean validate() {
+        boolean valid = true;
+
+        String username = this.username.getText().toString();
+        String password = this.password.getText().toString();
+
+        if (username.isEmpty()) {
+            this.username.setError("Nie wpisano nazwy użytkownika.");
+            valid = false;
+        } else {
+            this.username.setError(null);
+        }
+
+        if (password.isEmpty()) {
+            this.password.setError("Nie wpisano hasła.");
+            valid = false;
+        } else {
+            this.password.setError(null);
+        }
+
+        return valid;
+    }
+
+    /**
+     * TODO
+     */
+    public void loginFailed() {
+        Toast.makeText(getBaseContext(), "Logowanie nieudane.", Toast.LENGTH_LONG).show();
+        loginButton.setEnabled(true);
+    }
+
+    /**
+     * TODO
+     */
+    public void loginSuccess() {
+        Toast.makeText(getBaseContext(), "Logowanie udane.", Toast.LENGTH_LONG).show();
+        loginButton.setEnabled(true);
+    }
+
+    /**
+     * Otwiera aktywność rejestracja
+     *
+     * @param view aktualny interfejs
+     */
+    public void openRegister(View view) {
+        startActivity(new Intent(this, RegisterActivity.class));
+    }
+
+}
